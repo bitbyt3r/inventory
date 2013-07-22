633b94b7a50ec84484a8a7683f128fe5850bf7a4
mark25@UMBC.EDU <mark25@UMBC.EDU@bb2173db-233b-63cc-8cb0-ec26fdc3f785>
2013-07-22 15:13:42 +0000

    Working version

--- inventory.py
+++ inventory.py
@@ -4,20 +4,36 @@ import sys
 import db
 
 try:
-    con = _mysql.connect(db.server, db.user, db.password, db.database)
+  con = _mysql.connect(db.server, db.user, db.password, db.database)
         
-    con.query("SELECT VERSION()")
-    result = con.use_result()
+  con.query("SELECT VERSION()")
+  result = con.use_result()
     
-    print "MySQL version: %s" % \
-        result.fetch_row()[0]
+  print "MySQL version: %s" % result.fetch_row()[0]
     
 except _mysql.Error, e:
   
-    print "Error %d: %s" % (e.args[0], e.args[1])
-    sys.exit(1)
+  print "Error %d: %s" % (e.args[0], e.args[1])
+  sys.exit(1)
 
 finally:
-    
+  if con:
+    con.close()
+
+def validCode(code):
+  return True
+
+barcode = raw_input(":")
+while validCode(barcode):
+  try:
+    con = _mysql.connect(db.server, db.user, db.password, db.database)
+
+    con.query("insert into %s set idtag=%s" % (db.table, barcode))
+  except _mysql.Error, e:
+    print "Error %d: %s" % (e.args[0], e.args[1])
+    sys.exit(1)
+  finally:
     if con:
-        con.close()
+      con.close()
+
+  barcode = raw_input(":")
